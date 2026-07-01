0675121990255a760c6c0f6de007df35f318fbe0
sumit gupta <sumitgupta9579@gmail.com>
2026-07-01 20:09:38 +0530

    Time: 8 ms (6.09%), Space: 43.6 MB (5.41%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -0,0 +1,81 @@
+class Solution {
+    public static boolean isSafe(char [][] board , int row , int col){
+        int n =board.length;
+        // check row
+        for(int j=0;j<n;j++){
+            if(board[row][j]=='Q') return false;
+        }
+         // check col
+        for(int i=0;i<n;i++){
+            if(board[i][col]=='Q') return false;
+        }
+        // check north-east 
+        int i=row;
+        int j=col;
+        while (i>=0 && j<n) {
+            if(board[i][j]=='Q') return false;
+            i--;
+            j++;
+        }
+        // check south-east 
+         i=row;
+         j=col;
+        while (i<n && j<n) {
+            if(board[i][j]=='Q') return false;
+            i++;
+            j++;
+        }
+        // check south-west
+        i=row;
+        j=col;
+        while (i<n && j>=0) {
+            if(board[i][j]=='Q') return false;
+            i++;
+            j--;
+        }
+        // check north-west
+        i=row;
+        j=col;
+        while (i>=0 && j>=0) {
+            if(board[i][j]=='Q') return false;
+            i--;
+            j--;
+        }
+        return true ;
+    }
+
+    public static void nQueen( char [][] board , int row ,  List<List<String>> ans){
+        int n = board.length;
+        if(row==n){
+            List<String> l = new ArrayList<>();
+            for(int i=0;i<n;i++){
+                String str ="";
+                for(int j=0;j<n;j++){
+                    str += board[i][j];
+                }
+                l.add(str);
+            }
+            ans.add(l);
+            return;
+        }
+        for(int j=0;j<n;j++){
+            if(isSafe(board,row,j)){
+                board[row][j]='Q';
+                nQueen(board, row+1,ans);
+                board[row][j]='.';
+            }
+        }
+    }
+    public int totalNQueens(int n) {
+        char [][] board = new char[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                board[i][j]='.';
+
+            }
+        }
+        List<List<String>> ans = new ArrayList<>();
+        nQueen(board,0 ,ans);
+        return ans.size() ;
+    }
+}
