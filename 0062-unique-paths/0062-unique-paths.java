d901b6b8387d91cc3c8eaca77dbc0794c5ca954d
sumit gupta <sumitgupta9579@gmail.com>
2026-07-06 23:33:45 +0530

    Time: 0 ms (100%), Space: 42 MB (82.12%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -5,23 +5,24 @@ class Solution {
         
     // }
 
-    public int paths(int cr , int cc , int lr , int lc , int[][] dp) {
-       if(cr==lr && cc==lc) return 1 ;
-       if(cr>lr || cc >lc) return 0;
-       if(dp[cr][cc]!=-1) return dp[cr][cc];
-       int right = paths(cr,cc+1,lr,lc,dp);
-       int down = paths(cr+1,cc,lr,lc,dp);
-       return dp[cr][cc]=right + down ;
-    }
+    // public int paths(int cr , int cc , int lr , int lc , int[][] dp) {
+    //    if(cr==lr && cc==lc) return 1 ;
+    //    if(cr>lr || cc >lc) return 0;
+    //    if(dp[cr][cc]!=-1) return dp[cr][cc];
+    //    int right = paths(cr,cc+1,lr,lc,dp);
+    //    int down = paths(cr+1,cc,lr,lc,dp);
+    //    return dp[cr][cc]=right + down ;
+    // }
 
     public int uniquePaths(int m, int n) {
         int [][] dp = new int[m][n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                dp[i][j]=-1;
+                if(i==0 || j==0) dp[i][j]=1;
+                else dp[i][j]=dp[i][j-1]+dp[i-1][j];
             }
         }
-        return paths(0,0,m-1,n-1,dp);
+        return dp[m-1][n-1];
         
     }
 }
