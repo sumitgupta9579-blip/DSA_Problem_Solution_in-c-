633fa700bb28ca43a7bbb8e212f8adfcd6a3db0e
sumit gupta <sumitgupta9579@gmail.com>
2026-07-07 11:54:33 +0530

    Time: 0 ms (100%), Space: 41.7 MB (93.19%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -10,12 +10,14 @@ class Solution {
     public int fib(int n) {
     //     dp=new int[n+1];
     //    return fibo(n);
-    if(n<=1) return n;
-    int [] dp= new int[n+1];
-    dp[0]=0;dp[1]=1;
-    for(int i=2;i<=n;i++){
-        dp[i]=dp[i-1]+dp[i-2];
-    }
-    return dp[n];
+        if(n<=1) return n;
+        int [] dp= new int[3];
+        dp[0]=0;dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[2]=dp[0]+dp[1];
+            dp[0]=dp[1];
+            dp[1]=dp[2];
+        }
+        return dp[2];
     }
 }
