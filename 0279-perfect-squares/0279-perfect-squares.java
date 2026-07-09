276bdccebab343b9532f56f41aea45138f85d739
sumit gupta <sumitgupta9579@gmail.com>
2026-07-09 11:59:46 +0530

    Time: 195 ms (9.5%), Space: 44.7 MB (40.53%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -3,19 +3,31 @@ class Solution {
         int sqrt =(int)(Math.sqrt(n));
         return (sqrt*sqrt==n);
     }
+    public int minSquares(int n ,int []dp){
+        if(isPerfect(n)) return 1;
+        if(dp[n]!=-1) return dp[n];
+        int min =Integer.MAX_VALUE;
+        for(int i=1;i*i<=n;i++){
+            int count =minSquares(i*i,dp) +minSquares(n-i*i,dp);
+            min=Math.min(min,count);
+        }
+        return dp[n]=min;
+    }
     public int numSquares(int n) {
         int [] dp = new int[n+1];
-        for(int i=1;i<=n;i++){
-            if(isPerfect(i)) dp[i]=1;
-            else{
-                int min =Integer.MAX_VALUE;
-                for(int j=1;j*j<=i;j++){
-                    int count =dp[j*j]+dp[i-j*j];
-                    min=Math.min(min,count);
-                }
-                dp[i]=min;
-            }
-        }
-        return dp[n];
+        // for(int i=1;i<=n;i++){
+        //     if(isPerfect(i)) dp[i]=1;
+        //     else{
+        //         int min =Integer.MAX_VALUE;
+        //         for(int j=1;j*j<=i;j++){
+        //             int count =dp[j*j]+dp[i-j*j];
+        //             min=Math.min(min,count);
+        //         }
+        //         dp[i]=min;
+        //     }
+        // }
+        // return dp[n];
+        Arrays.fill(dp,-1);
+        return minSquares(n,dp);
     }
 }
