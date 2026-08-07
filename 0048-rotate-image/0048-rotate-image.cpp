856afc4ff80d0a34e1627dac764a08164b427c4f
sumit gupta <sumitgupta9579@gmail.com>
2026-08-07 16:41:47 +0530

    Time: 0 ms (100%), Space: 10.1 MB (94.08%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& arr) {
+        int m = arr.size() , n = arr[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                // if(arr[i]==arr[j]) continue;
+                swap(arr[i][j],arr[j][i]);
+            }
+        }
+        // Reverse Each row
+        for(int i=0;i<n;i++){
+            reverse(arr[i].begin(),arr[i].end());
+        }
+    }
+};
